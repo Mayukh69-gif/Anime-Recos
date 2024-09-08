335c55ad79478344bac4f598f75ac70245e0dd4f
Mayukh <mayukhm796@gmail.com>
2024-09-08 11:14:42 +0530

    API calls added

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import express from "express";
-import axios from "axios";
+import express from 'express';
+import axios from 'axios';
+
 const app = express();
 const port = 3000;
 
+// Set up EJS
 app.set('view engine', 'ejs');
 
-app.get("/",(req,res)=>{
-    res.render("partials/index");
-})
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+// Fetch a random joke and render the homepage
+app.get('/', async (req, res) => {
+    try {
+        // Fetch a random joke from JokeAPI
+        const response = await axios.get('https://v2.jokeapi.dev/joke/Any?type=single');
+        const joke = response.data;
+        res.render('partials/index', { data: joke });
+    } catch (error) {
+        res.status(500).send('Error fetching joke from API');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
